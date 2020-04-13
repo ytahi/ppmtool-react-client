0172bcfee62e1d22d1f8ecb1d7d327a19ab7dd1c
yacine.tahi <yacine.tahi@neoxia.com>
2020-04-13 17:04:01 +0100

    create project from react

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initalState = {};
+const initialState = {};
 const middleware = [thunk];
 
 let store;
@@ -10,7 +10,7 @@ let store;
 if (window.navigator.userAgent.includes("Chrome")) {
   store = createStore(
     rootReducer,
-    initalState,
+    initialState,
     compose(
       applyMiddleware(...middleware),
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
@@ -20,7 +20,7 @@ if (window.navigator.userAgent.includes("Chrome")) {
 } else {
   store = createStore(
     rootReducer,
-    initalState,
+    initialState,
     compose(applyMiddleware(...middleware))
   );
 }
